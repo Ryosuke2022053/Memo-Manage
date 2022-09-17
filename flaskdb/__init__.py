9ecb3bb8cd03dc8964029d4b5ef313f80d7c6016
Ryosuke2022053 <s2022053@stu.musashino-u.ac.jp>
2022-09-17 11:37:45 +0900

    [modify]　initファイルの変更

--- flaskdb/__init__.py
+++ flaskdb/__init__.py
@@ -3,13 +3,13 @@ A Sample Web-DB Application for DB-DESIGN lecture
 Copyright (C) 2022 Yasuhiro Hayashi
 """
 from flask import Flask
-apps = Flask(__name__)
+apps = Flask(__name__, static_folder="controller/view/static", template_folder="controller/view/templates")
 apps.config.from_object("flaskdb.config")
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(apps)
 
-from flaskdb.dataaccess import DataAccess
+from flaskdb.repository.dataaccess import DataAccess
 import flaskdb.var as v
 da = DataAccess(v.HOSTNAME, v.PORT, v.DBNAME, v.USERNAME, v.PASSWORD)
 
@@ -20,7 +20,11 @@ csrf.init_app(apps)
 from flask_bootstrap import Bootstrap
 bs = Bootstrap(apps)
 
-CONTEXT_PATH = "/flaskdb"
-from flaskdb.views import app
-apps.register_blueprint(app, url_prefix=CONTEXT_PATH + "/")
+from flaskdb.controller.views import app
+from flaskdb.controller.auth import auth_module
+from flaskdb.controller.item import item_module
+
+apps.register_blueprint(app)
+apps.register_blueprint(auth_module)
+apps.register_blueprint(item_module)
 
