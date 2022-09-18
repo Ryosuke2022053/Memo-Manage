f5eb28c5cfbc0d3256f510f9b5ce7f27280f8fc9
Ryosuke2022053 <s2022053@stu.musashino-u.ac.jp>
2022-09-18 15:10:13 +0900

    [add]　ファイルの重複を防ぐ機能を追加

--- flaskdb/controller/memo.py
+++ flaskdb/controller/memo.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, session, render_template, redirect, flash,
 
 from flaskdb import apps, db, da
 from flaskdb.service.memoMDE import memo_MDE
-from flaskdb.service.mainForm import file_rename
+from flaskdb.service.mainForm import file_rename, file_name_list
 
 memo_module = Blueprint("memo", __name__)
 
@@ -20,7 +20,14 @@ def memo_edit(file):
     if request.method == "POST":
         content = request.form["data"] 
         title = request.form["title"]
+        # ファイル名変更チェック
         if title != file:
+            # 重複チェック
+            mdfile_list = file_name_list()
+            if title in mdfile_list:
+                memo_MDE(file).write_md(content)
+                return render_template("memo/memo_edit.html", data=content, file=file, errortext = True)    
+            
             file_rename(file, title)
         memo_MDE(title).write_md(content)
         return render_template("memo/memo_edit.html", data=content, file=title)
@@ -32,7 +39,10 @@ def memo_edit(file):
 def memo_add():
     if request.method == "POST":
         title = request.form["title"]
-        content = request.form["data"]    
+        content = request.form["data"] 
+        mdfile_list = file_name_list()
+        if title in mdfile_list:
+            return render_template("memo/memo_add.html", data=content, file="", errortext = True)
         memo_MDE(title).write_md(content)
         return render_template("memo/memo_add.html", data=content, file=title)
     else:
--- flaskdb/service/memoMDE.py
+++ flaskdb/service/memoMDE.py
@@ -1,6 +1,8 @@
 from flask import Markup
 from markdown import markdown
 import os
+from flaskdb.controller.auth import now
+from flaskdb.service.mainForm import file_name_list, now_dir
 
 class memo_MDE:
   mdfile = None
@@ -9,21 +11,28 @@ class memo_MDE:
     self.mdfile = file
 
   def write_md(self, data):
-    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='w') as mdfile:
+    with open(now_dir() + '/' + self.mdfile + '.md', mode='w') as mdfile:
       mdfile.write(data)
 
-
   def read_md(self) -> str:
-    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='r') as mdfile:
+    with open(now_dir() + '/' + self.mdfile + '.md', mode='r') as mdfile:
       mdcontent = mdfile.read()
       return Markup(markdown(mdcontent))
 
-
   def read_edit_md(self) -> str:
-    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='r') as mdfile:
+    with open(now_dir() + '/' + self.mdfile + '.md', mode='r') as mdfile:
       mdcontent = mdfile.read()
       return mdcontent
   
   def delete_md(self):
-    os.remove('flaskdb/controller/view/article/' + self.mdfile + '.md')
+    os.remove(now_dir() + '/' + self.mdfile + '.md')
 
+
+  def check_file(self):
+    mdfile_list = file_name_list()
+    if self.mdfile in mdfile_list:
+      print('重複')
+      return True
+    else:
+      print('重複してない')
+      return False
