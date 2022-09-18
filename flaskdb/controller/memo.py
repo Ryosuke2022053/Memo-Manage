d5fb6ba8104706698fe4091b8d2ad9f2fdbf2d57
Ryosuke2022053 <s2022053@stu.musashino-u.ac.jp>
2022-09-18 13:59:29 +0900

    [modify]　クラスの導入

--- flaskdb/controller/memo.py
+++ flaskdb/controller/memo.py
@@ -2,7 +2,7 @@ from turtle import title
 from flask import Blueprint, request, session, render_template, redirect, flash, url_for, Markup
 
 from flaskdb import apps, db, da
-from flaskdb.service.memoMDE import delete_md, write_md, read_md, read_edit_md
+from flaskdb.service.memoMDE import memo_MDE
 from flaskdb.service.mainForm import file_rename
 
 memo_module = Blueprint("memo", __name__)
@@ -10,21 +10,20 @@ memo_module = Blueprint("memo", __name__)
 
 @memo_module.route("/view/<string:file>", methods=["GET"])
 def memo_view(file):
-    content = read_md(file)
-    print(file)
+    content = memo_MDE(file).read_md()
     return render_template('memo/memo_view.html', md=content, file=file)
 
 
 @memo_module.route("/edit/<string:file>", methods=["GET", "POST"])
 def memo_edit(file):
-    content = read_edit_md(file)
+    content = memo_MDE(file).read_edit_md()
     if request.method == "POST":
-      content = request.form["data"] 
-      title = request.form["title"]
-      if title != file:
-        file_rename(file, title)
-      write_md(content, title)
-      return render_template("memo/memo_edit.html", data=content, file=title)
+        content = request.form["data"] 
+        title = request.form["title"]
+        if title != file:
+            file_rename(file, title)
+        memo_MDE(title).write_md(content)
+        return render_template("memo/memo_edit.html", data=content, file=title)
     else:
         return render_template("memo/memo_edit.html", data=content, file=file)
 
@@ -34,12 +33,14 @@ def memo_add():
     if request.method == "POST":
         title = request.form["title"]
         content = request.form["data"]    
-        write_md(content, title)
+        memo_MDE(title).write_md(content)
         return render_template("memo/memo_add.html", data=content, file=title)
     else:
         return render_template("memo/memo_add.html", file="")
 
+
 @memo_module.route("/delete/<string:file>", methods=["GET"])
 def memo_delete(file):
-    delete_md(file)
+    memo_MDE(file).delete_md()
     return redirect(url_for("app.index"))
+
--- flaskdb/service/memoMDE.py
+++ flaskdb/service/memoMDE.py
@@ -2,21 +2,28 @@ from flask import Markup
 from markdown import markdown
 import os
 
-def write_md(data, mdfile):
-  with open('flaskdb/controller/view/article/' + mdfile + '.md', mode='w') as mdfile:
-    mdfile.write(data)
+class memo_MDE:
+  mdfile = None
+
+  def __init__(self, file=None):
+    self.mdfile = file
+
+  def write_md(self, data):
+    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='w') as mdfile:
+      mdfile.write(data)
 
 
-def read_md(mdfile) -> str:
-  with open('flaskdb/controller/view/article/' + mdfile + '.md', mode='r') as mdfile:
-    mdcontent = mdfile.read()
-    return Markup(markdown(mdcontent))
+  def read_md(self) -> str:
+    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='r') as mdfile:
+      mdcontent = mdfile.read()
+      return Markup(markdown(mdcontent))
 
 
-def read_edit_md(mdfile) -> str:
-  with open('flaskdb/controller/view/article/' + mdfile + '.md', mode='r') as mdfile:
-    mdcontent = mdfile.read()
-    return mdcontent
+  def read_edit_md(self) -> str:
+    with open('flaskdb/controller/view/article/' + self.mdfile + '.md', mode='r') as mdfile:
+      mdcontent = mdfile.read()
+      return mdcontent
+  
+  def delete_md(self):
+    os.remove('flaskdb/controller/view/article/' + self.mdfile + '.md')
 
-def delete_md(mdfile):
-  os.remove('flaskdb/controller/view/article/' + mdfile + '.md')
